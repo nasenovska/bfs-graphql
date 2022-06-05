15d66dc2082bbaee69c1edcffafb258e456139f9
Nina Asenovska <nina.asenovska@gmail.com>
2022-06-05 03:25:25 +0300

    Fixes

--- src/common/schema.ts
+++ src/common/schema.ts
@@ -6,13 +6,15 @@ import { FootballTeamResolver } from '../resolvers/football-team/football-team.r
 import { UserResolver } from '../resolvers/user/user.resolver';
 import { TypegooseMiddleware } from './typegoose-middleware';
 import { ObjectIdScalar } from './object-id.scalar';
+import { authChecker } from './auth-checker';
 
 export const getSchema = async () => {
     return await buildSchema({
         resolvers: [UserResolver, GameResolver, FootballTeamResolver],
         emitSchemaFile: path.resolve(__dirname, 'schema.gql'),
         globalMiddlewares: [TypegooseMiddleware],
-        scalarsMap: [{type: ObjectId, scalar: ObjectIdScalar}]
+        scalarsMap: [{type: ObjectId, scalar: ObjectIdScalar}],
+        authChecker: authChecker
     });
 }
 
